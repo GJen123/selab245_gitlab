bbd10aa584c6e832f2b81506e3ef3accf4f29b93
root <admin@example.com>
2017-08-03 13:11:51 +0800

    fix jenkinsapi test

--- src/test/java/jenkins/JenkinsApiTest.java
+++ src/test/java/jenkins/JenkinsApiTest.java
@@ -9,12 +9,6 @@ public class JenkinsApiTest {
   String jobUrl = "http://140.134.26.71:38080/job/HelloMaven/api/json";
 
   @Test
-  public void testLastBuildNum() {
-    String lastBuildUrl = jenkins.getLastBuildUrl("GJen", "zxcv1234", jobUrl);
-    System.out.println("lastBuildUrl : " + lastBuildUrl);
-  }
-
-  @Test
   public void testReadConsoleOutput() {
     String console = jenkins.getConsoleText("http://140.134.26.71:38080/job/HelloMaven/22/consoleText");
     System.out.println("console : " + console);
