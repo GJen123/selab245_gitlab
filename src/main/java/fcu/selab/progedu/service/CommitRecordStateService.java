b98674c6d89502ed4a99402a5a05c5a6680b6960
yee-xiang <blowball85830@gmail.com>
2018-03-21 13:32:43 +0800

    new method to update state table

--- src/main/java/fcu/selab/progedu/service/CommitRecordStateService.java
+++ src/main/java/fcu/selab/progedu/service/CommitRecordStateService.java
@@ -1,11 +1,10 @@
 package fcu.selab.progedu.service;
 
-//public class CommitRecordStateService {
-//
-//}
-
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,13 +15,18 @@ import javax.ws.rs.core.Response;
 
 import org.json.JSONObject;
 
+import fcu.selab.progedu.db.CommitRecordDbManager;
 import fcu.selab.progedu.db.CommitRecordStateDbManager;
 import fcu.selab.progedu.db.IDatabase;
 import fcu.selab.progedu.db.MySqlDatabase;
+import fcu.selab.progedu.db.ProjectDbManager;
 
 @Path("commits/")
 public class CommitRecordStateService {
-  CommitRecordStateDbManager commitRecordStatedDb = CommitRecordStateDbManager.getInstance();
+  CommitRecordStateDbManager commitRecordStateDb = CommitRecordStateDbManager.getInstance();
+  CommitRecordDbManager commitRecordDb = CommitRecordDbManager.getInstance();
+  ProjectDbManager projectDb = ProjectDbManager.getInstance();
+
   IDatabase database = new MySqlDatabase();
   Connection connection = database.getConnection();
 
@@ -37,12 +41,58 @@ public class CommitRecordStateService {
   @Path("state/")
   @Produces(MediaType.APPLICATION_JSON)
   public Response getCommitRecordStateCounts(@QueryParam("state") String state) {
-    List<Integer> array = commitRecordStatedDb.getCommitRecordStateCounts(connection, state);
+    List<Integer> array = commitRecordStateDb.getCommitRecordStateCounts(connection, state);
     JSONObject ob = new JSONObject();
     ob.put("data", array);
     ob.put("name", state);
     Response response = Response.ok().entity(ob.toString()).build();
     return response;
+  }
+
+  public void updateCommitRecordState() {
+
+    List<String> lsNames = new ArrayList<String>();
+    lsNames = projectDb.listAllProjectNames();
+
+    for (String name : lsNames) {
+
+      Map<String, Integer> map = new HashMap<>();
+
+      map = commitRecordDb.getCommitRecordStateCounts(name);
+
+      int success = 0;
+      int nb = 0;
+      int ctf = 0;
+      int csf = 0;
+      int cpf = 0;
+      int ccs = 0;
+
+      if (map.containsKey("S")) {
+        success = map.get("S");
+      }
+
+      if (map.containsKey("NB")) {
+        nb = map.get("NB");
+      }
+
+      if (map.containsKey("CTF")) {
+        ctf = map.get("CTF");
+      }
+
+      if (map.containsKey("CSF")) {
+        csf = map.get("CSF");
+      }
+
+      if (map.containsKey("CPF")) {
+        cpf = map.get("CPF");
+      }
+
+      ccs = success + ctf + csf + cpf;
+
+      commitRecordStateDb.addCommitRecordState(name, success, csf, cpf, ctf, nb, ccs);
+
+    }
+
   }
 
 }
